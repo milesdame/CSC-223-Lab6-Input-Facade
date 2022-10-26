6e3a1f4351ca1e3c3228ae9bb5584abf84b554bf
reganrichardson <92763866+reganrichardson@users.noreply.github.com>
2022-10-26 15:34:56 -0400

    InputFacade FINISHED NOT TESTED

--- src/input/InputFacade.java
+++ src/input/InputFacade.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.AbstractMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import geometry_objects.points.Point;
 import geometry_objects.points.PointDatabase;
+import geometry_objects.points.PointNamingFactory;
 import geometry_objects.Segment;
 import input.builder.GeometryBuilder;
 import input.components.ComponentNode;
@@ -47,6 +50,35 @@ public class InputFacade
 	 */
 	public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(String filename)
 	{
-		// TODO
+		FigureNode figNode = extractFigure(filename);
+		PointDatabase pointData = new PointDatabase();
+		LinkedHashSet<Segment> segData = new LinkedHashSet<Segment>();
+		
+		
+		//Get PointNode set
+		Set<PointNode> PND = figNode.getPointsDatabase().getPoints();
+		//for each pointNode in set, convert to point and add to pointDatabase
+		
+		for (PointNode pointNode : PND) {
+			
+			pointData.put(pointNode.getName(),pointNode.getX(),pointNode.getY());
+		}
+		
+		//Create segment list
+		List<SegmentNode> SNG = figNode.getSegments().asSegmentList();
+		
+		//for segments in SegmentNode, convert PointNodes to Points, then SegmentNode to Segment
+		for (SegmentNode SegNode: SNG) {
+			
+			Point point1 = new Point(SegNode.getPoint1().getX(), SegNode.getPoint1().getY());
+			Point point2 = new Point(SegNode.getPoint2().getX(), SegNode.getPoint2().getY());
+			
+			Segment Seg = new Segment(point1,point2);
+			segData.add(Seg);
+		}
+		
+		
+		Map.Entry<PointDatabase, Set<Segment>> newMap = new AbstractMap.SimpleEntry<>(pointData, segData);
+		return newMap;
 	}
 }
